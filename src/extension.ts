552a30481fa0e70d0b80907a32224e2c878c28ed
Michael Hunter <elec.mike@gmail.com>
2020-04-24 18:20:22 +1000

    Inline session being created

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,29 @@
 import * as vscode from 'vscode';
+import { LoggingDebugSession } from 'vscode-debugadapter';
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Activated microvium-debug');
-	let disposable = vscode.commands.registerCommand('microvium-debug.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from test!');
-  });
-	context.subscriptions.push(disposable);
+  console.log('actuvated!')
+  context.subscriptions.push(vscode.commands.registerCommand('microvium-debug.helloWorld', () => {
+    vscode.window.showInformationMessage('Hello World from test!');
+  }));
+
+  const factory: vscode.DebugAdapterDescriptorFactory = {
+		createDebugAdapterDescriptor(
+      session: vscode.DebugSession,
+      executable: vscode.DebugAdapterExecutable | undefined
+    ): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
+      console.log('x createDebugAdapterDescriptor');
+		  return new vscode.DebugAdapterInlineImplementation(new MicroviumDebugSession());
+    }
+  };
+	context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('microviumdebug', factory));
 }
 
 export function deactivate() {}
+
+class MicroviumDebugSession extends LoggingDebugSession {
+  constructor() {
+    super();
+    console.log('MicroviumDebugSession ctr')
+  }
+}
