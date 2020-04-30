5c6b4a1d9751cc195caebab49b640fca89a2e2bb
Raf <nicdaoraf@gmail.com>
2020-04-30 22:01:04 +1000

    WIP extension: stop-on-entry, stepping and stack traces appear to be somewhat working Expected to be locally linked to the microvium lib to start up

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,9 @@ import { DebugProtocol } from 'vscode-debugprotocol';
 import { basename } from 'path';
 import { SimpleRuntime, MockBreakpoint } from './simple-runtime';
 import { unexpected } from './utils';
+import * as fs from 'fs';
+import { VirtualMachineFriendly } from 'microvium';
+import { EventEmitter } from 'events';
 const { Subject: AwaitNotifySubject } = require('await-notify');
 
 const threadId = 1;
@@ -50,61 +53,11 @@ network), so it might help to reference this documentation:
 */
 
 class MicroviumDebugSession extends LoggingDebugSession {
-  private runtime = new SimpleRuntime();
-
   private configurationDone = new AwaitNotifySubject();
-  // TODO | HIGH | Raf: What are these?
-  private variableHandles = new Handles<string>();
-
-  // TODO | HIGH | Raf: Think about how to handle commands that potentially jump
-  // from 1 file to another like `continue`, `step`
-  /** Currently, SimpleRuntime assumes that there is only 1 file being debugged
-  (i.e. no references to other files). */
-  private entryPointFile: string | undefined = undefined;
+  private debuggerEventEmitter = new EventEmitter();
 
   constructor() {
     super("microvium-debug.txt");
-
-    // setup event handlers
-    this.runtime.on('stopOnEntry', () => {
-      this.sendEvent(new StoppedEvent('entry', threadId));
-    });
-    this.runtime.on('stopOnStep', () => {
-      this.sendEvent(new StoppedEvent('step', threadId));
-    });
-    this.runtime.on('stopOnBreakpoint', () => {
-      this.sendEvent(new StoppedEvent('breakpoint', threadId));
-    });
-    this.runtime.on('stopOnDataBreakpoint', () => {
-      this.sendEvent(new StoppedEvent('data breakpoint', threadId));
-    });
-    this.runtime.on('stopOnException', () => {
-      this.sendEvent(new StoppedEvent('exception', threadId));
-    });
-    this.runtime.on('breakpointValidated', (bp: MockBreakpoint) => {
-      this.sendEvent(new BreakpointEvent('changed', <DebugProtocol.Breakpoint>{ verified: bp.verified, id: bp.id }));
-    });
-    this.runtime.on('output', (text, filePath, line, column) => {
-      const e: DebugProtocol.OutputEvent = new OutputEvent(`${text}\n`);
-
-      if (text === 'start' || text === 'startCollapsed' || text === 'end') {
-        e.body.group = text;
-        e.body.output = `group-${text}\n`;
-      }
-
-      e.body.source = this.createSource(filePath);
-      e.body.line = this.convertDebuggerLineToClient(line);
-      e.body.column = this.convertDebuggerColumnToClient(column);
-      this.sendEvent(e);
-    });
-    this.runtime.on('end', () => {
-      this.sendEvent(new TerminatedEvent());
-    });
-  }
-
-  // TODO | HIGH | Raf: What does this do, exactly?
-  private createSource(filePath: string): Source {
-    return new Source(basename(filePath), this.convertDebuggerPathToClient(filePath), undefined, undefined, 'mock-adapter-data');
   }
 
   /**
@@ -112,6 +65,7 @@ class MicroviumDebugSession extends LoggingDebugSession {
    * to interrogate the features the debug adapter provides.
    */
   protected initializeRequest(response: DebugProtocol.InitializeResponse, args: DebugProtocol.InitializeRequestArguments) {
+    // console.log('Init args', JSON.stringify(args, null, 2));
     this.sendResponse(response);
     this.sendEvent(new InitializedEvent());
   }
@@ -120,13 +74,27 @@ class MicroviumDebugSession extends LoggingDebugSession {
     logger.init(event => console.log('Event: ', event.body && event.body.output));
     logger.setup(Logger.LogLevel.Verbose);
 
+    // It seems like Babel(?) starts lines at 1. Also, I can't figure out how to
+    // set InitializeRequestArguments.linesStartAt1 or columnsStartAt1 (luckily
+    // they both default to 1 so things just work out). The debugger lines and
+    // columns start at 0 by default and luckily they're set-able here, so
+    // again, things just work out for us :D
+    this.setDebuggerLinesStartAt1(true);
+    this.setDebuggerColumnsStartAt1(true);
+
     // Wait until configuration has finished (and configurationDoneRequest has been called)
     await this.configurationDone.wait(1000);
 
-    this.entryPointFile = args.program;
-    this.runtime.start(args.program, !!args.stopOnEntry);
+    this.debuggerEventEmitter.on('from-app:stop-on-entry', () => this.sendEvent(new StoppedEvent('entry', threadId)));
+    this.debuggerEventEmitter.on('from-app:stop-on-step', () => this.sendEvent(new StoppedEvent('step', threadId)));
+    this.debuggerEventEmitter.on('from-app:stop-on-breakpoint', () => this.sendEvent(new StoppedEvent('breakpoint', threadId)));
 
-    this.sendResponse(response);
+    const vm = new VirtualMachineFriendly(undefined, {}, {}, this.debuggerEventEmitter);
+    try {
+      vm.importNow({ sourceText: fs.readFileSync(args.program, 'utf8'), debugFilename: args.program });
+    } catch (e) {
+      console.error(e);
+    }
   }
 
   /**
@@ -153,40 +121,70 @@ class MicroviumDebugSession extends LoggingDebugSession {
     this.sendResponse(response);
   }
 
-  protected stackTraceRequest(response: DebugProtocol.StackTraceResponse, args: DebugProtocol.StackTraceArguments): void {
+  protected async stackTraceRequest(response: DebugProtocol.StackTraceResponse, args: DebugProtocol.StackTraceArguments) {
+    this.debuggerEventEmitter.emit('from-debugger:stack-request');
 
-    const startFrame = typeof args.startFrame === 'number' ? args.startFrame : 0;
-    const maxLevels = typeof args.levels === 'number' ? args.levels : 1000;
-    const endFrame = startFrame + maxLevels;
+    const stackFrames = await new Promise<any[]>(resolve => {
+      this.debuggerEventEmitter.once('from-app:stack', resolve);
+    });
 
-    const stack = this.runtime.stack(this.entryPointFile!, startFrame, endFrame);
+    // console.log('debugger:stack-received');
+    // console.log(JSON.stringify(stackFrames, null, 2));
 
     response.body = {
-      stackFrames: stack.frames.map((f: any) => new StackFrame(f.index, f.name, this.createSource(f.file), this.convertDebuggerLineToClient(f.line))),
-      totalFrames: stack.count
+      stackFrames: stackFrames.map((data, i) =>
+        new StackFrame(
+          i + (args.startFrame || 0),
+          basename(data.filePath),
+          new Source(
+            basename(data.filePath),
+            this.convertDebuggerPathToClient(data.filePath),
+            undefined,
+            undefined,
+            '{test-data}'
+          ),
+          this.convertDebuggerLineToClient(data.line),
+          this.convertDebuggerColumnToClient(data.column)
+        )
+      ),
+      totalFrames: stackFrames.length
     };
+
+    // console.log('debugger:stack-response');
+    // console.log(JSON.stringify(response.body.stackFrames, null, 2));
     this.sendResponse(response);
   }
 
-  protected continueRequest(response: DebugProtocol.ContinueResponse, args: DebugProtocol.ContinueArguments): void {
-    this.runtime.continue(this.entryPointFile!, 'forwards');
+  protected scopesRequest(response: DebugProtocol.ScopesResponse, args: DebugProtocol.ScopesArguments, request?: DebugProtocol.Request): void {
+    response.body = {
+      scopes: [{
+        name: '{scope}',
+        variablesReference: 0,
+        expensive: false
+      }]
+    }
     this.sendResponse(response);
   }
 
-  protected reverseContinueRequest(response: DebugProtocol.ReverseContinueResponse, args: DebugProtocol.ReverseContinueArguments): void {
-    this.runtime.continue(this.entryPointFile!, 'backwards');
+  // protected continueRequest(response: DebugProtocol.ContinueResponse, args: DebugProtocol.ContinueArguments): void {
+  //   this.runtime.continue(this.entryPointFile!, 'forwards');
+  //   this.sendResponse(response);
+  // }
+
+  // protected reverseContinueRequest(response: DebugProtocol.ReverseContinueResponse, args: DebugProtocol.ReverseContinueArguments): void {
+  //   this.runtime.continue(this.entryPointFile!, 'backwards');
+  //   this.sendResponse(response);
+  // }
+
+  protected async nextRequest(response: DebugProtocol.NextResponse, args: DebugProtocol.NextArguments) {
+    this.debuggerEventEmitter.emit('from-debugger:step-request');
+
     this.sendResponse(response);
   }
 
-  protected nextRequest(response: DebugProtocol.NextResponse, args: DebugProtocol.NextArguments): void {
-    this.runtime.step(this.entryPointFile!, 'forwards');
-    this.sendResponse(response);
-  }
-
-  protected stepBackRequest(response: DebugProtocol.StepBackResponse, args: DebugProtocol.StepBackArguments): void {
-    this.runtime.step(this.entryPointFile!, 'backwards');
-    this.sendResponse(response);
-  }
+  // protected stepBackRequest(response: DebugProtocol.StepBackResponse, args: DebugProtocol.StepBackArguments): void {
+  //   this.sendResponse(response);
+  // }
 
   // protected setBreakPointsRequest(response: DebugProtocol.SetBreakpointsResponse, args: DebugProtocol.SetBreakpointsArguments): void {
   //   const file = args.source.path || unexpected();
